8f5e94c89c85eab77b59d719df46f8632733c18c
ashpaxman <fitsintheblank@gmail.com>
2016-12-20 15:52:49 -0500

    Update GUI_Rename File, better looking, doesn't work.py

--- GUI_Rename File, better looking, doesn't work.py
+++ GUI_Rename File, better looking, doesn't work.py
@@ -18,26 +18,25 @@ def load_file():
     os.chdir(filename)
     print (os.getcwd())
 
-#Rename function
-def rename():
-##   i = 0
-##   for pdf in os.listdir():
-##      file_name, file_extension = os.path.splitext(file)
-##      i += 1
-##      new_file_name = 'prefix.get(){}.pdf'.format(i)
-##      os.rename(file, new_file_name)
+def rename_file(self, event=None, value=None):
+    if value is None:
+        value = self.get()
+    if self.command is not None:
+        if value == self.old_value:
+            return
+        self.command(value)
+        self.old_value = value
+    self.select_all()
 
-    for address in addresses:
-        for page in pages:
-            filename.append("{}_{}.pdf".format(address, page))
-
-    for i, name in enumerate(filename):
-        output = PdfFileWriter()
-        output.addPage(inputpdf.getPage(i))
-        with open(name, 'wb') as f:
-            output.write(f)   
-
-#Build tkinterGUI
+def set(self, text=None, run=False):
+    if text is None:
+        text = ""
+    self.delete(0, tk. END)
+    self.insert(0, str(text))
+    if run:
+        self.input_change(text)
+    self.old_value = text
+    
 self = Tk()
 self.minsize(450,100)
 self.title("Renamer")
@@ -50,22 +49,20 @@ target_file.set("")
 
 address = StringVar()
 page1 = StringVar()
-page2 = StringVar()
-page3 = StringVar()
 
-ttk.Button(mainframe, text="Select PDF", command=load_file).grid(column=1, row=1)
-ttk.Label(mainframe, textvariable=target_file).grid(column=2, row=1)
+ttk.Button(self, text="Select PDF", command=load_file).grid(column=1, row=1)
+ttk.Label(self, textvariable=target_file).grid(column=2, row=1)
 
-ttk.Label(mainframe, text='Addresses: (use comma to separate multiple)').grid(column=1, row=3, sticky=E)
-name_box = Entry(mainframe, width=50, textvariable=address)
+ttk.Label(self, text='Addresses: (use comma to separate multiple)').grid(column=1, row=3, sticky=E)
+name_box = Entry(self, width=50, textvariable=address)
 name_box.grid(column=2, row=3, sticky=W, padx=10)
 
-ttk.Label(mainframe, text='Pages: (use comma to separate multiple)').grid(column=1, row=4, sticky=E)
-name_box = Entry(mainframe, width=50, textvariable=page1)
+ttk.Label(self, text='Pages: (use comma to separate multiple)').grid(column=1, row=4, sticky=E)
+name_box = Entry(self, width=50, textvariable=page1)
 name_box.grid(column=2, row=4, sticky=W, padx=10)
 
-ttk.Label(mainframe).grid(column=1, row=7)
-ttk.Button(mainframe, text="Rename", command=rename).grid(column=1, row=7)
-ttk.Button(mainframe, text="Cancel", command=quit).grid(column=2, row=7, sticky=W)
+ttk.Label(self).grid(column=1, row=7)
+ttk.Button(self, text="Rename", command=rename_file).grid(column=1, row=7)
+ttk.Button(self, text="Cancel", command=quit).grid(column=2, row=7, sticky=W)
 
 self.mainloop()
